03170033f00ea5aa43ebbabeb3ff54702d684022
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-11-20 17:50:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,4 +11,21 @@ recognition.onresult = function(event){
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
+    speak();
 }
+
+function speak(){
+      var synth = window.SpeechSynthesis;
+      speak_data = document.getElementById("textbox").value;
+      var utterThis = new.SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+      Webcam.attach(camera); 
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+ });
