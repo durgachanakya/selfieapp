d6e43a3e36e64eab3e269710998c57458834bce3
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2021-11-21 17:48:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,21 +11,44 @@ recognition.onresult = function(event){
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    speak();
+    if(Content == "take my selfie"){
+        console.log("taking selfie ---");
+        speak();
+    }
+    
 }
 
 function speak(){
-      var synth = window.SpeechSynthesis;
-      speak_data = document.getElementById("textbox").value;
-      var utterThis = new.SpeechSynthesisUtterance(speak_data);
+      var synth = window.speechSynthesis;
+      speak_data = "taking your selfie in 5 seconds";
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
       synth.speak(utterThis);
       Webcam.attach(camera); 
+      setTimeout(function(){
+          take_snapshot();
+          save();
+      }, 5000);
 }
 
-camera = document.getElementById("camera");
+
 Webcam.set({
     width: 360,
     height: 250,
     image_format: 'jpeg',
     jpeg_quality: 90
  });
+
+ camera = document.getElementById("camera");
+
+ function take_snapshot(){
+     Webcam.snap(function(data_uri){
+         document.getElementById("result").innerHTML = '<img id= "selfie_image" src= "'+data_uri+'">';
+     });
+ }
+
+ function save(){
+     link = document.getElementById("link");
+     image = document.getElementById("selfie_image").src;
+     link.href = image;
+     link.click();
+ }
